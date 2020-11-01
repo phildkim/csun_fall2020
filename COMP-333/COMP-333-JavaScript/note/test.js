bacf854d3d1d03aee00fd8ad2219dc7d778d0d11
phildkim <philipdanielkim@gmail.com>
2020-11-01 05:28:34 -0800

    update

--- COMP-333/COMP-333-JavaScript/note/test.js
+++ COMP-333/COMP-333-JavaScript/note/test.js
@@ -10,7 +10,7 @@ Sub1.prototype.method = function () {
   console.log("sub1");
 }
 Sub2.prototype.method = function () {
-  console.log("sub2");
+  base.method();
 }
 let base = new Base();
 let sub1 = new Sub1();
@@ -39,54 +39,19 @@ console.log(sub2 instanceof Base); // prints "true"
   As a hint, you should not modify AddThis' prototype.
 */
 function AddThis(x) { this.x = x; }
-function SubThis(x) { this.x = x; }
-function MulThis(x) { this.x = x; }
+function AddThis(x) { this.x = x; }
+function AddThis(x) { this.x = x; }
 AddThis.prototype.add = function (y) { return this.x + y; }
-SubThis.prototype = new AddThis();
-SubThis.prototype.sub = function (y) { return this.x - y; }
-MulThis.prototype = new AddThis();
-MulThis.prototype.mul = function (y) { return this.x * y; }
+AddThis.prototype = new AddThis();
+AddThis.prototype.sub = function (y) { return this.x - y; }
+AddThis.prototype = new AddThis();
+AddThis.prototype.mul = function (y) { return this.x * y; }
 let withOne = new AddThis(1);
 let withFive = new AddThis(5);
-let subOne = new SubThis(withOne.add(2));
-let mulOne = new MulThis(subOne.sub(1));
+let subOne = new AddThis(withOne.add(2));
+let mulOne = new AddThis(subOne.sub(1));
 console.log(withOne.add(1));//2
 console.log(withOne.add(2));//3
 console.log(subOne.sub(1));//2
-console.log(mulOne.mul(5));//10
+console.log(mulOne.mul(withFive.add(1)));//12
 
-// join
-/*
-function join(list, delim) {
-  var retval = "[";
-  while (list instanceof Cons &&
-    !(list.tail instanceof Nil)) {
-    retval += list.head.toString() + delim;
-    list = list.tail;
-  }
-  if (list instanceof Cons) {
-    retval += list.head.toString();
-  }
-  retval += "]";
-  return retval;
-}; // join
-
-function List() { };
-List.prototype.join = function (delim) {
-  return join(this, delim);
-};
-List.prototype.toString = function () {
-  return this.join(", ");
-};// List
-*/
-// Parameters:
-// - A List of elements
-// - A delimeter to separate them by
-// Returns a single string, which results from calling
-// toString on each element, separated by the delimeter.
-//
-// For example:
-// join(new Nil(), ", ")                     // "[]"
-// join(new Cons(1, new Nil()), ", ")        // [1]
-// join(new Cons(2, new Cons(3, new Nil())), // [2, 3]
-//
--- README.md
+++ README.md
@@ -6,16 +6,22 @@
 - [X] ~~Due September 27th, Midnight : Assignment 2, 2-3 pages on Nobel Price.~~
 - [X] ~~Due September 22th, Extra Credit (zoom conference)~~
 - [X] ~~Midterm October 12th~~
+- [ ] Due November 15 Katherine Johnson Quiz
+- [ ] Final Paper
+- [ ] ***FINAL***
 
 ## COMP 256
 
 - [X] ~~Midterm October 12th~~
+- [ ] ***FINAL***
 
 ## COMP 333
 
 - [X] ~~<https://kyledewey.github.io/comp333-fall20/assignments/assign_1/>~~
 - [X] ~~Midterm October 1st~~
 - [X] ~~<https://kyledewey.github.io/comp333-fall20/assignments/assign_2/>~~
+- [ ] ***MIDTERM: November 12***
+- [ ] ***FINAL***
 
 ## COMP 490
 
