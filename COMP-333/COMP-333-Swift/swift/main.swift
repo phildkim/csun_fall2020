9420f10aded42207806171ea0d90fa412accbcb5
phildkim <philipdanielkim@gmail.com>
2020-12-01 21:43:14 -0800

    swift

--- COMP-333/COMP-333-Swift/swift/main.swift
+++ COMP-333/COMP-333-Swift/swift/main.swift
@@ -9,3 +9,12 @@ indirect enum MyBool {
   case Cons(Bool, MyBool)
   case Nil
 }
+
+func length(_ list: MyList) -> Int {
+  switch list {
+    case .Nil:
+      return 0
+    case .Cons(_, let tail):
+      return 1 + length(tail)
+  }
+}
